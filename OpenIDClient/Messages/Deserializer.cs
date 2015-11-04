82b1d4b80a76dea88578e13600c43f7b23c89be4
Andrea Biancini <andrea.biancini@gmail.com>
2015-11-04 10:43:01 +0100

    Refactred deserializer

--- OpenIDClient/Messages/Deserializer.cs
+++ OpenIDClient/Messages/Deserializer.cs
@@ -10,155 +10,187 @@
 
     public class Deserializer
     {
+        private static Dictionary<Type, Delegate> ParsersPerType = new Dictionary<Type, Delegate>()
+        {
+            { typeof(string), (Action<OIDClientSerializableMessage, PropertyInfo, object>) ParseString },
+            { typeof(bool), (Action<OIDClientSerializableMessage, PropertyInfo, object>)  ParseBool },
+            { typeof(int), (Action<OIDClientSerializableMessage, PropertyInfo, object>) ParseInteger },
+            { typeof(ResponseType), (Action<OIDClientSerializableMessage, PropertyInfo, object>) ParseResponseType },
+            { typeof(List<string>), (Action<OIDClientSerializableMessage, PropertyInfo, object>) ParseListString },
+            { typeof(List<ResponseType>), (Action<OIDClientSerializableMessage, PropertyInfo, object>) ParseListResponseType },
+            { typeof(Dictionary<string, object>), (Action<OIDClientSerializableMessage, PropertyInfo, object>) ParseDictionaryStringObject },
+            { typeof(DateTime), (Action<OIDClientSerializableMessage, PropertyInfo, object>) ParseDateTime },
+            { typeof(OIDCClientRegistrationRequest), (Action<OIDClientSerializableMessage, PropertyInfo, object>) ParseOIDCMessage },
+            { typeof(OIDCAuthorizationRequestMessage), (Action<OIDClientSerializableMessage, PropertyInfo, object>) ParseOIDCMessage },
+            { typeof(OIDCThirdPartyLoginRequest), (Action<OIDClientSerializableMessage, PropertyInfo, object>) ParseOIDCMessage },
+            { typeof(OIDCAuthCodeResponseMessage), (Action<OIDClientSerializableMessage, PropertyInfo, object>) ParseOIDCMessage },
+            { typeof(OIDCAuthImplicitResponseMessage), (Action<OIDClientSerializableMessage, PropertyInfo, object>) ParseOIDCMessage },
+            { typeof(OIDCTokenRequestMessage), (Action<OIDClientSerializableMessage, PropertyInfo, object>) ParseOIDCMessage },
+            { typeof(OIDCResponseWithToken), (Action<OIDClientSerializableMessage, PropertyInfo, object>) ParseOIDCMessage },
+            { typeof(OIDCClientSecretJWT), (Action<OIDClientSerializableMessage, PropertyInfo, object>) ParseOIDCMessage },
+            { typeof(OIDCTokenResponseMessage), (Action<OIDClientSerializableMessage, PropertyInfo, object>) ParseOIDCMessage },
+            { typeof(OIDCUserInfoRequestMessage), (Action<OIDClientSerializableMessage, PropertyInfo, object>) ParseOIDCMessage },
+            { typeof(OIDCUserInfoResponseMessage), (Action<OIDClientSerializableMessage, PropertyInfo, object>) ParseOIDCMessage },
+            { typeof(OIDCIdToken), (Action<OIDClientSerializableMessage, PropertyInfo, object>) ParseOIDCMessage },
+            { typeof(OIDClaimData), (Action<OIDClientSerializableMessage, PropertyInfo, object>) ParseOIDCMessage },
+            { typeof(OIDClaims), (Action<OIDClientSerializableMessage, PropertyInfo, object>) ParseOIDCMessage },
+            { typeof(OIDCResponseError), (Action<OIDClientSerializableMessage, PropertyInfo, object>) ParseOIDCMessage },
+            { typeof(OIDCProviderMetadata), (Action<OIDClientSerializableMessage, PropertyInfo, object>) ParseOIDCMessage },
+            { typeof(OIDCClientInformation), (Action<OIDClientSerializableMessage, PropertyInfo, object>) ParseOIDCMessage },
+            { typeof(OIDCKey), (Action<OIDClientSerializableMessage, PropertyInfo, object>) ParseOIDCMessage },
+            { typeof(OIDCAddress), (Action<OIDClientSerializableMessage, PropertyInfo, object>) ParseOIDCMessage }
+        };
+
+        private static void ParseString(OIDClientSerializableMessage obj, PropertyInfo p, object value)
+        {
+            string propertyValue = (string)value;
+            p.SetValue(obj, propertyValue);
+        }
+
+        private static void ParseResponseType(OIDClientSerializableMessage obj, PropertyInfo p, object value)
+        {
+            if (value.GetType() == typeof(ResponseType))
+            {
+                p.SetValue(obj, (ResponseType)value);
+            }
+            else
+            {
+                switch (value.ToString())
+                {
+                    case "code":
+                        p.SetValue(obj, ResponseType.Code);
+                        break;
+                    case "token":
+                        p.SetValue(obj, ResponseType.Token);
+                        break;
+                    case "id_token":
+                        p.SetValue(obj, ResponseType.Token);
+                        break;
+                }
+            }
+        }
+
+        private static void ParseListString(OIDClientSerializableMessage obj, PropertyInfo p, object value)
+        {
+            List<string> propertyValue = new List<string>();
+            if (value.GetType() == typeof(string))
+            {
+                propertyValue.Add((string)value);
+            }
+            else
+            {
+                dynamic arrayData = value;
+                foreach (string val in arrayData)
+                {
+                    propertyValue.Add(val);
+                }
+            }
+
+            p.SetValue(obj, propertyValue);
+        }
+
+        private static void ParseListResponseType(OIDClientSerializableMessage obj, PropertyInfo p, object value)
+        {
+            List<ResponseType> propertyValue = new List<ResponseType>();
+            if (value.GetType() == typeof(ResponseType))
+            {
+                propertyValue.Add((ResponseType)value);
+            }
+            else if (value.GetType() == typeof(string))
+            {
+                switch (value.ToString())
+                {
+                    case "code":
+                        propertyValue.Add(ResponseType.Code);
+                        break;
+                    case "token":
+                        propertyValue.Add(ResponseType.Token);
+                        break;
+                    case "id_token":
+                        propertyValue.Add(ResponseType.Token);
+                        break;
+                }
+            }
+            else if (value.GetType() == typeof(List<ResponseType>))
+            {
+                List<ResponseType> arrayData = (List<ResponseType>)value;
+                foreach (ResponseType val in arrayData)
+                {
+                    propertyValue.Add(val);
+                }
+            }
+            else
+            {
+                JArray arrayData = (JArray)value;
+                foreach (JValue val in arrayData)
+                {
+                    if (!new List<string> { "code", "token", "id_token" }.Contains(val.ToString()))
+                    {
+                        continue;
+                    }
+                    propertyValue.Add(val.ToObject<ResponseType>());
+                }
+            }
+
+            p.SetValue(obj, propertyValue);
+        }
+
+        private static void ParseDictionaryStringObject(OIDClientSerializableMessage obj, PropertyInfo p, object value)
+        {
+            Dictionary<string, object> propertyValue = (Dictionary<string, object>)value;
+            p.SetValue(obj, propertyValue);
+        }
+
+        private static void ParseDateTime(OIDClientSerializableMessage obj, PropertyInfo p, object value)
+        {
+            long dataLong = long.Parse(string.Empty + value);
+            DateTime propertyValue = DateTime.MaxValue;
+            if (dataLong != 0)
+            {
+                propertyValue = SecondsUtcToDateTime(dataLong);
+            }
+
+            p.SetValue(obj, propertyValue);
+        }
+
+        private static void ParseBool(OIDClientSerializableMessage obj, PropertyInfo p, object value)
+        {
+            bool propertyValue = bool.Parse(string.Empty + value);
+            p.SetValue(obj, propertyValue);
+        }
+
+        private static void ParseInteger(OIDClientSerializableMessage obj, PropertyInfo p, object value)
+        {
+            int propertyValue = int.Parse(string.Empty + value);
+            p.SetValue(obj, propertyValue);
+        }
+
+        private static void ParseOIDCMessage(OIDClientSerializableMessage obj, PropertyInfo p, object value)
+        {
+            OIDClientSerializableMessage propertyValue = (OIDClientSerializableMessage)Activator.CreateInstance(p.PropertyType);
+            JObject curObj = (JObject)value;
+            propertyValue.DeserializeFromDictionary(curObj.ToObject<Dictionary<string, object>>());
+            p.SetValue(obj, propertyValue);
+        }
+
         public static void DeserializeFromDictionary(OIDClientSerializableMessage obj, Dictionary<string, object> data)
         {
             PropertyInfo[] properties = obj.GetType().GetProperties(BindingFlags.Public | BindingFlags.Instance);
 
             foreach (PropertyInfo p in properties)
             {
-                if (!OIDClientSerializableMessage.IsSupportedType(p.PropertyType))
-                {
-                    continue;
-                }
-
                 string propertyCamel = p.Name;
                 string propertyUnderscore = Regex.Replace(propertyCamel, "(?<=.)([A-Z])", "_$0", RegexOptions.Compiled).ToLower();
+                Type propertyType = p.PropertyType;
+                object propertyValue = (data.ContainsKey(propertyUnderscore)) ? data[propertyUnderscore] : null;
 
-                try
-                {
-                    if (data[propertyUnderscore] == null)
-                    {
-                        continue;
-                    }
-                }
-                catch (KeyNotFoundException)
+                if (!ParsersPerType.ContainsKey(p.PropertyType) || propertyValue == null)
                 {
                     continue;
                 }
 
-                if (p.PropertyType == typeof(string))
-                {
-                    string propertyValue = (string)data[propertyUnderscore];
-                    p.SetValue(obj, propertyValue);
-                }
-                else if (p.PropertyType == typeof(ResponseType))
-                {
-                    if (data[propertyUnderscore].GetType() == typeof(ResponseType))
-                    {
-                        p.SetValue(obj, (ResponseType)data[propertyUnderscore]);
-                    }
-                    else
-                    {
-                        switch (data[propertyUnderscore].ToString())
-                        {
-                            case "code":
-                                p.SetValue(obj, ResponseType.Code);
-                                break;
-                            case "token":
-                                p.SetValue(obj, ResponseType.Token);
-                                break;
-                            case "id_token":
-                                p.SetValue(obj, ResponseType.Token);
-                                break;
-                        }
-                    }
-                }
-                else if (p.PropertyType == typeof(List<string>))
-                {
-                    List<string> propertyValue = new List<string>();
-                    if (data[propertyUnderscore].GetType() == typeof(string))
-                    {
-                        propertyValue.Add((string)data[propertyUnderscore]);
-                    }
-                    else
-                    {
-                        dynamic arrayData = data[propertyUnderscore];
-                        foreach (string val in arrayData)
-                        {
-                            propertyValue.Add(val);
-                        }
-                    }
-
-                    p.SetValue(obj, propertyValue);
-                }
-                else if (p.PropertyType == typeof(List<ResponseType>))
-                {
-                    List<ResponseType> propertyValue = new List<ResponseType>();
-                    if (data[propertyUnderscore].GetType() == typeof(ResponseType))
-                    {
-                        propertyValue.Add((ResponseType)data[propertyUnderscore]);
-                    }
-                    else if (data[propertyUnderscore].GetType() == typeof(string))
-                    {
-                        switch (data[propertyUnderscore].ToString())
-                        {
-                            case "code":
-                                propertyValue.Add(ResponseType.Code);
-                                break;
-                            case "token":
-                                propertyValue.Add(ResponseType.Token);
-                                break;
-                            case "id_token":
-                                propertyValue.Add(ResponseType.Token);
-                                break;
-                        }
-                    }
-                    else if (data[propertyUnderscore].GetType() == typeof(List<ResponseType>))
-                    {
-                        List<ResponseType> arrayData = (List<ResponseType>) data[propertyUnderscore];
-                        foreach (ResponseType val in arrayData)
-                        {
-                            propertyValue.Add(val);
-                        }
-                    }
-                    else
-                    {
-                        JArray arrayData = (JArray)data[propertyUnderscore];
-                        foreach (JValue val in arrayData)
-                        {
-                            if (!new List<string> { "code", "token", "id_token" }.Contains(val.ToString()))
-                            {
-                                continue;
-                            }
-                            propertyValue.Add(val.ToObject<ResponseType>());
-                        }
-                    }
-
-                    p.SetValue(obj, propertyValue);
-                }
-                else if (p.PropertyType == typeof(Dictionary<string, object>))
-                {
-                    Dictionary<string, object>propertyValue = (Dictionary<string, object>)data[propertyUnderscore];
-                    p.SetValue(obj, propertyValue);
-                }
-                else if (p.PropertyType == typeof(DateTime))
-                {
-                    long dataLong = long.Parse(string.Empty + data[propertyUnderscore]);
-                    DateTime propertyValue = DateTime.MaxValue;
-                    if (dataLong != 0)
-                    {
-                        propertyValue = SecondsUtcToDateTime(dataLong);
-                    }
-
-                    p.SetValue(obj, propertyValue);
-                }
-                else if (p.PropertyType == typeof(bool))
-                {
-                    bool propertyValue = bool.Parse(string.Empty + data[propertyUnderscore]);
-                    p.SetValue(obj, propertyValue);
-                }
-                else if (p.PropertyType == typeof(int))
-                {
-                    int propertyValue = int.Parse(string.Empty + data[propertyUnderscore]);
-                    p.SetValue(obj, propertyValue);
-                }
-                else if (typeof(OIDClientSerializableMessage).IsAssignableFrom(p.PropertyType))
-                {
-                    OIDClientSerializableMessage propertyValue = (OIDClientSerializableMessage) Activator.CreateInstance(p.PropertyType);
-                    JObject curObj = (JObject)data[propertyUnderscore];
-                    propertyValue.DeserializeFromDictionary(curObj.ToObject<Dictionary<string, object>>());
-                    p.SetValue(obj, propertyValue);
-                }
+                Delegate d = ParsersPerType[propertyType];
+                d.DynamicInvoke(obj, p, propertyValue);
             }
         }
 
